96dea4d4c82300757a563c03dab53da76cbf35ec
elidahayiemba <elidahayiemba@gmail.com>
2022-10-26 06:45:42 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Banner from './Component/banner'
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      
+    </div>
+  );
+}
+
+export default App;
